515e295b076fb50a8999e3d55a9e915feeedc77f
Plik129 <plikforo@gmail.com>
2021-10-20 15:09:08 +0000

    upload file project finished

--- server.js
+++ server.js
@@ -1,17 +1,34 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
-
+const bodyParser= require('body-parser')
+const multer = require('multer');
 var app = express();
 
 app.use(cors());
+app.use(bodyParser.urlencoded({extended: true}))
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+const router = express.Router();
+router.post('/fileanalyse', multer().single('upfile'), (req, res) => {
+  const file = req.file
+  if (!file) {
+    res.json({
+      error: "Please upload a file"
+    });
+  }else{    
+    res.json({
+      name: file.originalname,
+      type: file.mimetype,
+      size: file.size
+    });
+  }
+});
+app.use('/api', router);
 
 
 const port = process.env.PORT || 3000;
